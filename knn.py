50ec74fce98f266cf1d26c6b6c4935ab341f3094
aspencer422 <45663940+aspencer422@users.noreply.github.com>
2021-09-25 23:36:06 -0700

    Add files via upload

--- knn.py
+++ knn.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Anthony Spencer
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# FILENAME: Knn.py
+# SPECIFICATION: knn file for class
 # FOR: CS 4210- Assignment #2
 # TIME SPENT: 1 hour
 #-----------------------------------------------------------*/
